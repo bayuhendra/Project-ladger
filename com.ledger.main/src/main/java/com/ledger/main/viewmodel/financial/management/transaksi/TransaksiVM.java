aba857dd5ca413d06402152ed7166ff7bd72f2ab
bayuhendra <bayuhendra1078@gmail.com>
2018-02-14 07:48:33 +0700

    update modul transaksi

--- com.ledger.main/src/main/java/com/ledger/main/viewmodel/financial/management/transaksi/TransaksiVM.java
+++ com.ledger.main/src/main/java/com/ledger/main/viewmodel/financial/management/transaksi/TransaksiVM.java
@@ -45,13 +45,13 @@ import org.zkoss.zul.Window;
  */
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class TransaksiVM {
-
+    
     @WireVariable
     TransaksiService transaksiService;
-
+    
     private TransaksiDTO transaksiDTO = new TransaksiDTO();
     private List<TransaksiDTO> transaksiDTOs = new ArrayList<>();
-
+    
     private ListModelList<Kategori> kategoris = new ListModelList<>();
     private ListModelList<JenisBarang> jenises = new ListModelList<>();
     private ListModelList<StatusTransaksi> statusTransaksis = new ListModelList<>();
@@ -59,35 +59,35 @@ public class TransaksiVM {
 
     /* search option*/
     private Kategori kategori;
-
+    
     private PageNavigation previous;
     private boolean checked;
     private int pageSize = 9;
     private int activePage = 0;
     private int selectedIndex;
     private int totalSize = 0;
-
+    
     private String transaksiID;
     private String namaProduk;
-
+    
     @Init
     public void init(
             @ExecutionArgParam("transaksiDTO") TransaksiDTO transaksi,
             @ExecutionArgParam("previous") PageNavigation previous) {
-
+        
         initData();
-
+        
         checkValidity(transaksi, previous);
-
+        
     }
-
+    
     private void initData() {
         transaksiDTOs = transaksiService.findAll();
         if (transaksiDTOs.isEmpty()) {
             transaksiDTOs = Collections.emptyList();
         }
     }
-
+    
     private void checkValidity(TransaksiDTO transaksi, PageNavigation previous) {
         if (transaksi == null) {
             ListModelList<TransaksiDTO> parameterList = new ListModelList<>(transaksiService.findAll());
@@ -100,15 +100,18 @@ public class TransaksiVM {
             transaksiDTO = new TransaksiDTOBuilder()
                     .setTransaksiID(transaksiID)
                     .setCreatedBy(SecurityUtil.getUserName())
+                    .setTanggalTransaksi(new Date())
                     .setCreatedDate(new Date())
+                    .setStatusTransaksi(StatusTransaksi.IN_PROGRESS)
                     .createTransaksiDTO();
         } else {
             this.transaksiDTO = transaksi;
             transaksiID = transaksiDTO.getTransaksiID();
+            kategori = transaksiDTO.getKategoriTransaksi();
             this.previous = previous;
         }
     }
-
+    
     protected String getLatestObjectID(ListModelList list, String attribute) {
         int count = 0;
         int pointer = 0;
@@ -119,7 +122,7 @@ public class TransaksiVM {
             String att = attribute;
             String[] arr = new String[attribute.length()];
             String key = "";
-
+            
             if (att.contains(".")) {
                 arr = att.split("\\.");
                 int f = 1;
@@ -134,9 +137,9 @@ public class TransaksiVM {
             } else {
                 key = att;
             }
-
+            
             att = map.get(key).toString();
-
+            
             String temp = "";
             int countTemp = 0;
             for (int i = att.length(); i > 0; i--) {
@@ -153,10 +156,10 @@ public class TransaksiVM {
             }
             count = countTemp;
         }
-
+        
         return s + String.format("%0" + count + "d", max + 1);
     }
-
+    
     @Command("buttonCreateTransaksi")
     @NotifyChange("transaksi")
     public void detailPembelian(@BindingParam("object") TransaksiDTO obj, @ContextParam(ContextType.VIEW) Window window) {
@@ -164,13 +167,13 @@ public class TransaksiVM {
         params.put("transaksiDTO", obj);
         CommonViewModel.navigateToWithoutDetach("/financial-management/transaksi/add.transaksi.zul", window, params);
     }
-
+    
     @Command("buttonKembali")
     @NotifyChange({"transaksiDTO", "transaksiDTOs"})
     public void buttonKembaliCivitas(@BindingParam("object") TransaksiDTO obj, @ContextParam(ContextType.VIEW) Window window) {
         window.detach();
     }
-
+    
     @Command("buttonSimpanTransaksi")
     @NotifyChange("transaksiDTO")
     public void buttonSimpanTransaksi(@BindingParam("object") TransaksiDTO obj, @ContextParam(ContextType.VIEW) Window window) {
@@ -180,13 +183,13 @@ public class TransaksiVM {
         BindUtils.postGlobalCommand(null, null, "refreshData", null);
         window.detach();
     }
-
+    
     @GlobalCommand
     @NotifyChange("transaksiDTOs")
     public void refreshData() {
         transaksiDTOs = transaksiService.findAll();
     }
-
+    
     @Command("detailTransaksi")
     @NotifyChange("transaksi")
     public void detailTransaksi(@BindingParam("object") TransaksiDTO obj, @ContextParam(ContextType.VIEW) Window window) {
@@ -194,12 +197,12 @@ public class TransaksiVM {
         params.put("transaksiDTO", obj);
         CommonViewModel.navigateToWithoutDetach("/financial-management/transaksi/add.transaksi.zul", window, params);
     }
-
+    
     @Command("deleteTransaksi")
     @NotifyChange("transaksiDTOs")
     public void deleteTransaksi(@BindingParam("object") TransaksiDTO obj, @ContextParam(ContextType.VIEW) Window window) {
         transaksiDTO = (TransaksiDTO) obj;
-
+        
         Messagebox.show("Apakah anda yakin ingin menghapus Transaksi?", "Konfirmasi", Messagebox.OK | Messagebox.CANCEL, Messagebox.QUESTION,
                 new org.zkoss.zk.ui.event.EventListener() {
             @Override
@@ -214,135 +217,135 @@ public class TransaksiVM {
             }
         }
         );
-
+        
     }
-
+    
     public TransaksiService getTransaksiService() {
         return transaksiService;
     }
-
+    
     public void setTransaksiService(TransaksiService transaksiService) {
         this.transaksiService = transaksiService;
     }
-
+    
     public TransaksiDTO getTransaksiDTO() {
         return transaksiDTO;
     }
-
+    
     public void setTransaksiDTO(TransaksiDTO transaksiDTO) {
         this.transaksiDTO = transaksiDTO;
     }
-
+    
     public List<TransaksiDTO> getTransaksiDTOs() {
         return transaksiDTOs;
     }
-
+    
     public void setTransaksiDTOs(List<TransaksiDTO> transaksiDTOs) {
         this.transaksiDTOs = transaksiDTOs;
     }
-
+    
     public ListModelList<Kategori> getKategoris() {
         return new ListModelList<>(Kategori.values());
     }
-
+    
     public void setKategoris(ListModelList<Kategori> kategoris) {
         this.kategoris = kategoris;
     }
-
+    
     public ListModelList<JenisBarang> getJenises() {
         return new ListModelList<>(JenisBarang.values());
     }
-
+    
     public void setJenises(ListModelList<JenisBarang> jenises) {
         this.jenises = jenises;
     }
-
+    
     public ListModelList<StatusTransaksi> getStatusTransaksis() {
         return new ListModelList<>(StatusTransaksi.values());
     }
-
+    
     public void setStatusTransaksis(ListModelList<StatusTransaksi> statusTransaksis) {
         this.statusTransaksis = statusTransaksis;
     }
-
+    
     public ListModelList<JenisPembayaran> getJenisPembayarans() {
         return new ListModelList<>(JenisPembayaran.values());
     }
-
+    
     public void setJenisPembayarans(ListModelList<JenisPembayaran> jenisPembayarans) {
         this.jenisPembayarans = jenisPembayarans;
     }
-
+    
     public PageNavigation getPrevious() {
         return previous;
     }
-
+    
     public void setPrevious(PageNavigation previous) {
         this.previous = previous;
     }
-
+    
     public boolean isChecked() {
         return checked;
     }
-
+    
     public void setChecked(boolean checked) {
         this.checked = checked;
     }
-
+    
     public int getPageSize() {
         return pageSize;
     }
-
+    
     public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
-
+    
     public int getActivePage() {
         return activePage;
     }
-
+    
     public void setActivePage(int activePage) {
         this.activePage = activePage;
     }
-
+    
     public int getSelectedIndex() {
         return selectedIndex;
     }
-
+    
     public void setSelectedIndex(int selectedIndex) {
         this.selectedIndex = selectedIndex;
     }
-
+    
     public int getTotalSize() {
         return totalSize;
     }
-
+    
     public void setTotalSize(int totalSize) {
         this.totalSize = totalSize;
     }
-
+    
     public String getTransaksiID() {
         return transaksiID;
     }
-
+    
     public void setTransaksiID(String transaksiID) {
         this.transaksiID = transaksiID;
     }
-
+    
     public String getNamaProduk() {
         return namaProduk;
     }
-
+    
     public void setNamaProduk(String namaProduk) {
         this.namaProduk = namaProduk;
     }
-
+    
     public Kategori getKategori() {
         return kategori;
     }
-
+    
     public void setKategori(Kategori kategori) {
         this.kategori = kategori;
     }
-
+    
 }
